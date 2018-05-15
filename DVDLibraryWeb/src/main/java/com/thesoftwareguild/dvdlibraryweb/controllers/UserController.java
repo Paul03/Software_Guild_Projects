41ecc58375208f6a2cb612077d3724b75a78c980
Paul Harding (Mac) <paulharding90@gmail.com>
2018-05-14 21:13:39 -0400

    fix the request mapping for the UserController

--- DVDLibraryWeb/src/main/java/com/thesoftwareguild/dvdlibraryweb/controllers/UserController.java
+++ DVDLibraryWeb/src/main/java/com/thesoftwareguild/dvdlibraryweb/controllers/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping(value = "/user")
 public class UserController {
 
     private UserDao userDao;
