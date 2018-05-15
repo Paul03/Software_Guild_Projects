6fecfc49b86284cb2da68c8a1a1d3bba5011875a
Paul Harding (Mac) <paulharding90@gmail.com>
2018-05-14 20:36:24 -0400

    added noteDao dependency to DvdDaoDbImpl - still need to take noteDao out of DvdController

--- DVDLibraryWeb/src/main/java/com/thesoftwareguild/dvdlibraryweb/dao/DvdDaoDbImpl.java
+++ DVDLibraryWeb/src/main/java/com/thesoftwareguild/dvdlibraryweb/dao/DvdDaoDbImpl.java
@@ -4,6 +4,7 @@ import com.thesoftwareguild.dvdlibraryweb.dto.Dvd;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.thesoftwareguild.dvdlibraryweb.dto.Note;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +15,7 @@ import org.springframework.jdbc.core.RowMapper;
 public class DvdDaoDbImpl implements DvdDao {
 
     private JdbcTemplate jdbcTemplate;
+    private NoteDao noteDao;
 
     private static final String SQL_INSERT_DVD = "INSERT INTO dvd (title, release_date, mpaa_rating, director, studio, user_rating) VALUES(?, ?, ?, ?, ?, ?);";
     private static final String SQL_SELECT_DVD = "SELECT * FROM dvd WHERE id=?;";
@@ -25,12 +27,12 @@ public class DvdDaoDbImpl implements DvdDao {
     private static final String SQL_SELECT_DIRECTOR = "SELECT * FROM dvd WHERE director=?;";
     private static final String SQL_SELECT_STUDIO = "SELECT * FROM dvd WHERE studio=?;";
     private static final String SQL_SELECT_TITLE = "SELECT * FROM dvd WHERE title=?;";
-    private static final String SQL_AVERAGE_AGE = "SELECT YEAR(CURDATE()) - AVG( YEAR(release_date) ) FROM dvd;";
     private static final String SQL_SELECT_NEWEST_DVD = "SELECT * FROM dvd ORDER BY release_date desc;";
     private static final String SQL_SELECT_OLDEST_DVD = "SELECT * FROM dvd ORDER BY release_date asc;";
 
-    public DvdDaoDbImpl(JdbcTemplate jdbcTemplate) {
+    public DvdDaoDbImpl(JdbcTemplate jdbcTemplate, NoteDao noteDao) {
         this.jdbcTemplate = jdbcTemplate;
+        this.noteDao = noteDao;
     }
 
     @Override
@@ -48,13 +50,20 @@ public class DvdDaoDbImpl implements DvdDao {
         Integer newId = jdbcTemplate.queryForObject("SELECT LAST_INSERT_ID();", Integer.class);
         dvd.setDvdId(newId);
 
+        // Insert Note(s)
+        dvd.getUserNotes().forEach(note -> noteDao.insert(note));
+
         return dvd;
     }
 
     @Override
     public Dvd retrieve(long id) {
 
-        return jdbcTemplate.queryForObject(SQL_SELECT_DVD, new DVDMapper(), id);
+        Dvd dvd = jdbcTemplate.queryForObject(SQL_SELECT_DVD, new DVDMapper(), id);
+        List<Note> userNotes = noteDao.findByDvd(dvd);
+        dvd.setUserNotes(userNotes);
+
+        return dvd;
 
     }
 
@@ -70,6 +79,8 @@ public class DvdDaoDbImpl implements DvdDao {
                 dvd.getUserRating(),
                 dvd.getDvdId());
 
+        dvd.getUserNotes().forEach(note -> noteDao.update(note));
+
     }
 
     @Override
@@ -83,61 +94,89 @@ public class DvdDaoDbImpl implements DvdDao {
     @Override
     public List<Dvd> all() {
 
-        return jdbcTemplate.query(SQL_SELECT_ALL_DVD, new DVDMapper());
+        List<Dvd> dvds = jdbcTemplate.query(SQL_SELECT_ALL_DVD, new DVDMapper());
+        setUserNotes(dvds);
+
+        return dvds;
 
     }
 
     @Override
     public List<Dvd> searchLastNYears(int years) {
 
-        return jdbcTemplate.query(SQL_SELECT_LAST_N_YEARS, new DVDMapper(), years);
+        List<Dvd> dvds = jdbcTemplate.query(SQL_SELECT_LAST_N_YEARS, new DVDMapper(), years);
+        setUserNotes(dvds);
+
+        return dvds;
 
     }
 
     @Override
     public List<Dvd> searchByMPAARating(String rating) {
 
-        return jdbcTemplate.query(SQL_SELECT_MPAA_RATING, new DVDMapper(), rating);
+        List<Dvd> dvds = jdbcTemplate.query(SQL_SELECT_MPAA_RATING, new DVDMapper(), rating);
+        setUserNotes(dvds);
+
+        return dvds;
 
     }
 
     @Override
     public List<Dvd> searchByDirector(String directorName) {
 
-        return jdbcTemplate.query(SQL_SELECT_DIRECTOR, new DVDMapper(), directorName);
+        List<Dvd> dvds = jdbcTemplate.query(SQL_SELECT_DIRECTOR, new DVDMapper(), directorName);
+        setUserNotes(dvds);
+
+        return dvds;
 
     }
 
     @Override
     public List<Dvd> searchByStudio(String studioName) {
 
-        return jdbcTemplate.query(SQL_SELECT_STUDIO, new DVDMapper(), studioName);
+        List<Dvd> dvds = jdbcTemplate.query(SQL_SELECT_STUDIO, new DVDMapper(), studioName);
+        setUserNotes(dvds);
+
+        return dvds;
 
     }
 
     @Override
-    public Double averageAge() {
+    public List<Dvd> findNewestDvd() {
 
-        return jdbcTemplate.queryForObject(SQL_AVERAGE_AGE, Double.class);
+        List<Dvd> dvds = jdbcTemplate.query(SQL_SELECT_NEWEST_DVD, new DVDMapper());
+        setUserNotes(dvds);
+
+        return dvds;
     }
 
     @Override
-    public List<Dvd> findNewestDVD() {
+    public List<Dvd> findOldestDvd() {
 
-        return jdbcTemplate.query(SQL_SELECT_NEWEST_DVD, new DVDMapper());
-    }
+        List<Dvd> dvds = jdbcTemplate.query(SQL_SELECT_OLDEST_DVD, new DVDMapper());
+        setUserNotes(dvds);
 
-    @Override
-    public List<Dvd> findOldestDVD() {
-
-        return jdbcTemplate.query(SQL_SELECT_OLDEST_DVD, new DVDMapper());
+        return dvds;
 
     }
 
     @Override
     public List<Dvd> searchByTitle(String title) {
 
-        return jdbcTemplate.query(SQL_SELECT_TITLE, new DVDMapper(), title);
+        List<Dvd> dvds = jdbcTemplate.query(SQL_SELECT_TITLE, new DVDMapper(), title);
+        setUserNotes(dvds);
+
+        return dvds;
+
+    }
+
+    private void setUserNotes(List<Dvd> dvds) {
+
+        dvds.forEach(dvd -> {
+            List<Note> dvdNotes = noteDao.findByDvd(dvd);
+            dvd.setUserNotes(dvdNotes);
+        });
+
     }
 
     private static final class DVDMapper implements RowMapper<Dvd> {
