30bea59b8653a5ae3baf9b49443fc493202889f3
Paul Harding (Mac) <paulharding90@gmail.com>
2018-05-14 21:18:08 -0400

    setup LoginController

--- DVDLibraryWeb/src/main/java/com/thesoftwareguild/dvdlibraryweb/controllers/LoginController.java
+++ DVDLibraryWeb/src/main/java/com/thesoftwareguild/dvdlibraryweb/controllers/LoginController.java
@@ -1,4 +1,16 @@
 package com.thesoftwareguild.dvdlibraryweb.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value = "/login")
 public class LoginController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "login";
+    }
+
 }
---
+++ DVDLibraryWeb/src/main/webapp/jsp/login.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<html>
+
+<head>
+    <title>DVD Library | Login</title>
+</head>
+
+<body>
+</body>
+
+</html>
